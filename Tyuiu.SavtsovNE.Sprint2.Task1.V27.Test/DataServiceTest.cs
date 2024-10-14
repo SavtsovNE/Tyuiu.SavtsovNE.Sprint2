1a68aac0373670a4d4e9720226a064d88e2a15e4
Никита Савцов <nsavtsov89@inbox.ru>
2024-10-15 00:03:19 +0500

    Task1

---
+++ Tyuiu.SavtsovNE.Sprint2.Task1.V27.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+using Tyuiu.SavtsovNE.Sprint2.Task1.V27.Lib;
+
+namespace Tyuiu.SavtsovNE.Sprint2.Task1.V27.Test
+{
+    namespace Tyuiu.SavtsovNE.Sprint2.Task1.V27.Test
+    {
+        [TestClass]
+        public class DataServiceTest
+        {
+            [TestMethod]
+            public void ValidGetCompareOperations()
+            {
+                DataService ds = new DataService();
+                int a = 654;
+                int b = 325;
+                int c = 154;
+                int d = 333;
+                bool[] res = new bool[6];
+                res = ds.GetLogicOperations(a, b, c, d);
+                bool[] wait = new bool[6] { false, false, true, false, true, false };
+                CollectionAssert.AreEqual(wait, res);
+
+            }
+        }
+    }
+}
