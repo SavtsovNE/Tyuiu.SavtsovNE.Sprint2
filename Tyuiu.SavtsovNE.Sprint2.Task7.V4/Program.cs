65a303ea77a9c0c8f48b77804cb892c88fafd8a0
Никита Савцов <nsavtsov89@inbox.ru>
2024-10-16 22:36:43 +0500

    Task7

---
+++ Tyuiu.SavtsovNE.Sprint2.Task7.V4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SavtsovNE.Sprint2.Task7.V4.Lib;
+namespace Tyuiu.SavtsovNE.Sprint2.Task7.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Никита Савцов Евгеньевич | ИБКСб - 24 - 1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Никита Савцов Евгеньевич | ИБКСб - 24 - 1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на C#, которая запрашивает исходные                  *");
+            Console.WriteLine("* данные (вещественные значения) и вычисляет, находится ли точка с        *");
+            Console.WriteLine("* координатами X,Y в заштрихованной области.                              *");
+            Console.WriteLine("***************************************************************************");
+        }
+    }
+}
